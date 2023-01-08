7d7f33b64e797ad8c45e0ce3bf85825885e8cfc4
CodeLoverERLC22 <113789226+CodeLoverERLC22@users.noreply.github.com>
2023-01-08 18:15:38 +0000

    Add files via upload

---
+++ 97_Project Template-20230108T180057Z-001/97_Project Template/game_login.js
@@ -0,0 +1,12 @@
+// Create function addUser()
+function addUser() {
+  // Get value of user by id player1_name_input and player2_name_input
+document.getElementById("player1_name_input", player1_name).getValue;
+document.getElementById("player1_name_input", player2_name).getValue;
+  // Store these values locally
+set(player1_name).localStorage;
+set(player2_name).localStorage;
+  //Assign "game_page.html" to window.location
+  window.location(game_page.html);
+}
+
